9e6b6927f039d7306dab82ae874deafae78709d6
Scott Berk <itunes@spudfish.com>
2022-02-23 12:46:53 +0000

    added ability to manually move the train through a file semaphore

--- train_controller.js
+++ train_controller.js
@@ -2,9 +2,17 @@
 
 const Serial = require('serialport');
 const fetch = require('node-fetch');
+const fs = require('fs');
+
 const API_URL = 'https://api-v3.mbta.com/predictions?filter[stop]=place-portr&filter[route]=CR-Fitchburg';
 const INBOUND_THRESHHOLD = 20000;
-const OUTBOUND_THRESHHOLD = 30000;
+const OUTBOUND_THRESHHOLD = 65000;
+const QUERY_FREQUENCY = 15000;
+const ARDUINO_VENDOR_ID = 2341;
+const ARDUINO_BAUD_RATE = 115200;
+
+const INBOUND_SEMAPHORE = './i';
+const OUTBOUND_SEMAPHORE = './o';
 
 let train_direction;
 let port;
@@ -12,17 +20,17 @@ let port;
 async function init() {
   port = await init_serial();
   train_direction = 0;
-  setInterval(check_loop, 15000);
+  setInterval(check_loop, QUERY_FREQUENCY);
 }
 
 async function init_serial() {
   const device_list = await Serial.list();
-  const arduino = device_list.find(e => e.vendorId == 2341);
+  const arduino = device_list.find(e => e.vendorId == ARDUINO_VENDOR_ID);
 
   if (arduino && arduino.path) {
     console.log(`Connecting to Arduino at ${arduino.path}`);
     try {
-      const port = Serial(arduino.path, { baudRate: 115200 });
+      const port = Serial(arduino.path, { baudRate: ARDUINO_BAUD_RATE });
       return port;
     } catch (error) {
       console.log(`ERROR: Cannot open the port: ${error}`);
@@ -64,6 +72,24 @@ async function check_loop() {
       }
     }
   }
+
+  fs.access(INBOUND_SEMAPHORE, fs.F_OK, (err) => {
+    if (!err) {
+      console.log(`Sending the train INBOUND`);
+      port.write('i'); // if inbound semaphore detected, send the train inbound
+      fs.unlinkSync(INBOUND_SEMAPHORE);
+      train_direction = 0;
+    }
+  });
+
+  fs.access(OUTBOUND_SEMAPHORE, fs.F_OK, (err) => {
+    if (!err) {
+      console.log(`Sending the train OUTBOUND`);
+      port.write('o'); // if outbound semaphore detected, send the train outbound
+      fs.unlinkSync(OUTBOUND_SEMAPHORE);
+      train_direction = 1;
+    }
+  });
 }
 
 module.exports = { init };
